64155bceb4fd1648a5c0a0fa76412af89897f56e
Shriyash Rulhe <shriyashrulhe145@gmail.com>
2024-09-27 15:53:30 +0530

    frontend api update

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 const JWT_SECRET = process.env.JWT_SECRET;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://agriequip-green.vercel.app/',
   methods: ["POST", "Get"],
   credentials: true
 })); 
