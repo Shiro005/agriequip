b1599872aae871a033dae478b990831544bca71e
Shriyash Rulhe <shriyashrulhe145@gmail.com>
2024-09-27 14:20:53 +0530

    27sepupdate

--- frontend/src/components/SignIn/SignIn.jsx
+++ frontend/src/components/SignIn/SignIn.jsx
@@ -14,7 +14,7 @@ export function SignIn() {
         setError('');
 
         try {
-            const response = await fetch('https://agriequip-lilac.vercel.app/signin', {
+            const response = await fetch('http://localhost:5000/api/users/signin', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
