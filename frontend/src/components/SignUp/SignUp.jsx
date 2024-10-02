5a4e3e51738da07ee1b030d9a0af58b51a606d2b
Shriyash Rulhe <shriyashrulhe145@gmail.com>
2024-10-02 15:26:44 +0530

    frontend update

--- frontend/src/components/SignUp/SignUp.jsx
+++ frontend/src/components/SignUp/SignUp.jsx
@@ -16,7 +16,7 @@ export function SignUp() {
         setError('');
 
         try {
-            const response = await fetch('https://agriequip-backend-api.vercel.app/api/users/signup', {
+            const response = await fetch('https://agriequip-backend.vercel.app/api/users/signup', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
